e0381ade50501a750798e4a3cd52dcacafc7127d
Alessio Tortello <leccio22@alessios-macbook-pro.local>
2017-12-11 14:35:35 +0100

    :D

---
+++ app/app/Controller/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  FirstMini
+//
+//  Created by Alessio Tortello on 11/12/2017.
+//  Copyright © 2017 Alessio Tortello. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func button1(_ sender: Any) {
+    }
+    
+}
+
---
+++ app/app/Controller/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  FirstMini
+//
+//  Created by Alessio Tortello on 11/12/2017.
+//  Copyright © 2017 Alessio Tortello. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

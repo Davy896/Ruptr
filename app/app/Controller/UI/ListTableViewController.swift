537980b58b1436c40d6919f1f127822a40002aa7
LucasAssisRo <lucas.assis.ro@gmail.com>
2017-12-19 09:06:34 +0100

    starting to work on the map

--- app/app/Controller/UI/ListTableViewController.swift
+++ app/app/Controller/UI/ListTableViewController.swift
@@ -13,14 +13,6 @@ import SCLAlertView
 
 class ListTableViewController: ConnectivityViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var people: [UserProfile] = []
-    let alertAppearence = SCLAlertView.SCLAppearance(kCircleIconHeight: -56,
-                                                     kTitleFont: UIFont(name: "Futura-Bold", size: 17)!,
-                                                     kTextFont: UIFont(name: "Futura-Medium", size: 14)!,
-                                                     kButtonFont: UIFont(name: "Futura-Medium", size: 17)!,
-                                                     showCloseButton: false,
-                                                     showCircularIcon: true)
-    
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -39,7 +31,7 @@ class ListTableViewController: ConnectivityViewController, UITableViewDelegate, 
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        people.removeAll()
+        self.people.removeAll()
         self.tableView.reloadData()
     }
     
@@ -48,85 +40,13 @@ class ListTableViewController: ConnectivityViewController, UITableViewDelegate, 
     }
     
     override func peerFound(withId id: MCPeerID) {
-        self.updateFoundPeers()
+        super.peerFound(withId: id)
         self.tableView.reloadData()
     }
     
     override func peerLost(withId id: MCPeerID) {
-        if (self.people.count > 0) {
-            for i in 0 ... self.people.count - 1 {
-                if self.people[0].id == id.displayName.components(separatedBy: "|")[0] {
-                    self.people.remove(at: i)
-                    break
-                }
-            }
-            
-            self.tableView.reloadData()
-        }
-    }
-    
-    override func invitePeer(withId id: MCPeerID, profile: ProfileRequirements) {
-        if let userBeingInvited = profile as? UserProfile {
-            
-            let alert = SCLAlertView(appearance: self.alertAppearence)
-            let serviceBrowser = ServiceManager.instance.chatService.serviceBrowser
-            alert.addButton("Game") {
-                self.isGame = true
-                serviceBrowser.invitePeer(id,
-                                          to: ServiceManager.instance.chatService.session,
-                                          withContext: ListTableViewController.createUserData(for: "game"),
-                                          timeout: 20)
-            }
-            
-            alert.addButton("Chat") {
-                self.isGame = false
-                serviceBrowser.invitePeer(id,
-                                          to: ServiceManager.instance.chatService.session,
-                                          withContext: ListTableViewController.createUserData(for: "chat"),
-                                          timeout: 20)
-            }
-            
-            alert.addButton("Cancel", backgroundColor: UIColor.red) {
-                UIImpactFeedbackGenerator(style: UIImpactFeedbackStyle.light).impactOccurred()
-            }
-            
-            OperationQueue.main.addOperation {
-                alert.showInfo(userBeingInvited.username,
-                               subTitle: "Send invitation for",
-                               colorStyle: userBeingInvited.avatarSkin.toHexUInt(),
-                               circleIconImage: UIImage.imageByCombiningImage(firstImage: userBeingInvited.avatarFace!, withImage: userBeingInvited.avatarHair!))
-            }
-        }
-    }
-    
-    override func handleInvitation(from: MCPeerID, withContext context: Data?) {
-        if let context = context {
-            if let data = String(data: context, encoding: String.Encoding.utf8) {
-                
-                let chatService = ServiceManager.instance.chatService
-                let userData = ListTableViewController.decodeUserData(from: data)
-                let invitationText = userData[DecodedUserDataKeys.interactionType] == "chat" ? "chat." : "to play a game."
-                let alert = SCLAlertView(appearance: self.alertAppearence)
-                
-                alert.addButton("Accept") {
-                    self.isGame = userData[DecodedUserDataKeys.interactionType]! == "game"
-                    chatService.invitationHandler(true, chatService.session)
-                }
-                
-                alert.addButton("Refuse", backgroundColor: UIColor.red) {
-                    UIImpactFeedbackGenerator(style: UIImpactFeedbackStyle.heavy).impactOccurred()
-                }
-                
-                OperationQueue.main.addOperation {
-                    alert.showInfo(userData[DecodedUserDataKeys.username]!,
-                                   subTitle: "Is inviting you to \(invitationText)",
-                        colorStyle: Colours.getColour(named: userData[DecodedUserDataKeys.avatarSkinTone]!,
-                                                      index: Int(userData[DecodedUserDataKeys.avatarSkinToneIndex]!)).toHexUInt(),
-                        circleIconImage: UIImage.imageByCombiningImage(firstImage: UIImage(named: userData[DecodedUserDataKeys.avatarHair]!)!,
-                                                                       withImage: UIImage(named: userData[DecodedUserDataKeys.avatarFace]!)!))
-                }
-            }
-        }
+        super.peerLost(withId: id)
+        self.tableView.reloadData()
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -156,7 +76,7 @@ class ListTableViewController: ConnectivityViewController, UITableViewDelegate, 
                 for peer in ServiceManager.instance.chatService.peers {
                     if (peer.displayName.components(separatedBy: "|")[0] == profile.id) {
                         id = peer
-                        break 
+                        break
                     }
                 }
                 
@@ -167,52 +87,6 @@ class ListTableViewController: ConnectivityViewController, UITableViewDelegate, 
                 tableView.deselectRow(at: indexPath, animated: true)
             }
         }
-    }
-    
-    private func updateFoundPeers() {
-        self.people.removeAll()
-        let peers = ServiceManager.instance.chatService.peers
-        let infos = ServiceManager.instance.chatService.peersDiscoveryInfos
-        if peers.count > 0 {
-            for i in 0 ... peers.count - 1 {
-                self.people.append(UserProfile(id: peers[i].displayName.components(separatedBy: "|")[0],
-                                               username: infos[i][DecodedUserDataKeys.username.enumToString]!,
-                                               avatar: [AvatarParts.hair: infos[i][DecodedUserDataKeys.avatarHair.enumToString]!,
-                                                        AvatarParts.face: infos[i][DecodedUserDataKeys.avatarFace.enumToString]!,
-                                                        AvatarParts.skin: infos[i][DecodedUserDataKeys.avatarSkinTone.enumToString]!],
-                                               moods: [Mood.stringToEnum(from: infos[i][DecodedUserDataKeys.moodOne.enumToString]!),
-                                                       Mood.stringToEnum(from: infos[i][DecodedUserDataKeys.moodTwo.enumToString]!),
-                                                       Mood.stringToEnum(from:infos[i][DecodedUserDataKeys.moodThree.enumToString]!)],
-                                               status: Status.stringToEnum(from: infos[i][DecodedUserDataKeys.status.enumToString]!)))
-            }
-        }
-    }
-    
-    private static func createUserData(for interaction: String) -> Data {
-        let userProfile = ServiceManager.instance.userProfile
-        let data = "\(userProfile.username)|" +
-            "\(userProfile.avatar[AvatarParts.hair]!)|" +
-            "\(userProfile.avatar[AvatarParts.face]!)|" +
-            "\(userProfile.avatar[AvatarParts.skin]!)|" + // skinColour|index
-            "\(userProfile.moods[0].enumToString)|" +
-            "\(userProfile.moods[1].enumToString)|" +
-            "\(userProfile.moods[2].enumToString)|" +
-        "\(userProfile.status.enumToString)|"
-        return (interaction == "chat" ? data + "chat " : data + "game").data(using: String.Encoding.utf8)!  // has 10 components separeted by |
-    }
-    
-    private static func decodeUserData(from data: String) -> [DecodedUserDataKeys : String] {
-        let userData = data.components(separatedBy: "|")
-        return [.username: userData[0],
-                .avatarHair: userData[1],
-                .avatarFace: userData[2],
-                .avatarSkinTone: userData[3],
-                .avatarSkinToneIndex: userData[4],
-                .moodOne: userData[5],
-                .moodTwo: userData[6],
-                .moodThree: userData[7],
-                .status: userData[8],
-                .interactionType: userData[9]]
     }
 }
 
@@ -242,45 +116,3 @@ extension UIImage {
         return image!
     }
 }
-
-private enum DecodedUserDataKeys {
-    case username
-    case avatarHair
-    case avatarFace
-    case avatarSkinTone
-    case avatarSkinToneIndex
-    case moodOne
-    case moodTwo
-    case moodThree
-    case status
-    case interactionType
-    
-    var enumToString: String {
-        get {
-            switch self {
-            case DecodedUserDataKeys.username:
-                return "username"
-            case DecodedUserDataKeys.avatarHair:
-                return "avatarHair"
-            case DecodedUserDataKeys.avatarFace:
-                return "avatarFace"
-            case DecodedUserDataKeys.avatarSkinTone:
-                return "avatarSkinTone"
-            case DecodedUserDataKeys.avatarSkinToneIndex:
-                return "avatarSkinToneIndex"
-            case DecodedUserDataKeys.moodOne:
-                return "moodOne"
-            case DecodedUserDataKeys.moodTwo:
-                return "moodTwo"
-            case DecodedUserDataKeys.moodThree:
-                return "moodThree"
-            case DecodedUserDataKeys.status:
-                return "status"
-            case DecodedUserDataKeys.interactionType:
-                return "interactionType"
-            }
-        }
-    }
-}
-
-

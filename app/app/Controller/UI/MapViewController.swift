fd1652d42f5500dbabf2a9132a6814b7390fe07a
LucasAssisRo <lucas.assis.ro@gmail.com>
2017-12-25 11:52:54 +0100

    map moves

--- app/app/Controller/UI/MapViewController.swift
+++ app/app/Controller/UI/MapViewController.swift
@@ -12,20 +12,29 @@ import MultipeerConnectivity
 class MapViewController: ConnectivityViewController {
     
     var circleView: CircleView!
+    var circleCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         UIViewController.setViewBackground(for: self)
-        self.circleView = CircleView(frame: self.view.frame)
-        self.view.addSubview(self.circleView)
         self.title = NSLocalizedString("map", comment: "")
+        
+        
+        self.circleView = CircleView(frame: self.view.frame)
+        self.circleCenter = self.view.frame.origin
+        
+        circleView.isUserInteractionEnabled = true
+        
+        self.view.addSubview(self.circleView)
+        
         let a = AvatarPlanetButton(frame: CGRect(x: 50, y: 200, width: 40, height: 40))
         a.hairImageView.image = ServiceManager.instance.userProfile.avatarHair
         a.faceImageView.image = ServiceManager.instance.userProfile.avatarFace
         a.backgroundColor = ServiceManager.instance.userProfile.avatarSkin
         a.borderColor = UIColor.blue
         a.addTarget(self, action: #selector(showInvitationPrompt(_:)), for: UIControlEvents.touchUpInside)
+        
         self.view.addSubview(a)
         self.view.bringSubview(toFront: a)
         self.updateCircles(numberOfPeers: ServiceManager.instance.chatService.peers.count)
@@ -37,20 +46,6 @@ class MapViewController: ConnectivityViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         self.updateCircles(numberOfPeers: ServiceManager.instance.chatService.peers.count)
-    }
-    
-    func updateCircles(numberOfPeers: Int) {
-        var peersMissing: Int = numberOfPeers
-        var circlePopulation: Int = 1
-        var circleIndex: Int = 0
-        while peersMissing > 0 {
-            peersMissing -= circlePopulation
-            circlePopulation += (circleIndex + 1)
-            circleIndex += 1
-        }
-        
-        self.circleView.drawCircles(numberOf: circleIndex, onRectangle: circleView.frame, withRadius: 40)
-        self.circleView.setNeedsDisplay()
     }
     
     override func peerFound(withId id: MCPeerID) {
@@ -65,6 +60,57 @@ class MapViewController: ConnectivityViewController {
     
     @IBAction func showInvitationPrompt(_ sender: UIButton) {
         print("ndaklsjdkladjlksakldasjkldsajdklsjdlkas")
+    }
+    
+    @IBAction func moveCircles(_ sender: UIPanGestureRecognizer) {
+        self.circleView.isPanned = true
+        switch sender.state {
+        case UIGestureRecognizerState.began:
+            break
+        case UIGestureRecognizerState.changed:
+            let translation = sender.translation(in: self.view)
+            self.circleView.center.x = self.circleView.center.x + translation.x
+            self.circleView.center.y = self.circleView.center.y + translation.y
+            sender.setTranslation(CGPoint.zero, in: self.view)
+            break
+        case UIGestureRecognizerState.ended:
+            let velocity = sender.velocity(in: self.view)
+            let endPosition = CGPoint(x: self.circleView.center.x + (velocity.x * 0.1),
+                                      y: self.circleView.center.y + (velocity.y * 0.1))
+            UIView.animate(withDuration: 0.35,
+                           delay: 0,
+                           options: UIViewAnimationOptions.curveEaseOut,
+                           animations: { self.circleView.center = endPosition },
+                           completion: { finished in self.circleView.isPanned = false })
+            break
+        default:
+            break
+        }
+    }
+    
+    @IBAction func centerCircles(_ sender: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 0.35,
+                       delay: 0,
+                       options: UIViewAnimationOptions.curveEaseOut,
+                       animations: { self.circleView.center = self.view.center },
+                       completion: nil)
+    }
+    
+    func updateCircles(numberOfPeers: Int) {
+        var peersMissing: Int = numberOfPeers
+        var circlePopulation: Int = 1
+        var circleIndex: Int = 0
+        while peersMissing > 0 {
+            peersMissing -= circlePopulation
+            circlePopulation += (circleIndex + 1)
+            circleIndex += 1
+        }
+        
+        self.circleView.drawCircles(numberOf: circleIndex,
+                                    onRectangle: CGRect(origin: self.circleCenter,
+                                                        size: self.view.frame.size),
+                                    withRadius: 80)
+        self.circleView.setNeedsDisplay()
     }
 }
 
--- app/app/View/Sources/CircleView.swift
+++ app/app/View/Sources/CircleView.swift
@@ -12,34 +12,50 @@ class CircleView: UIView {
     
     private var radius: Double = 0
     private var numberOfCircles: Int = 0
-    var path: UIBezierPath!
-    var points: [(x: Double, y: Double)] = []
+    var points: [CGPoint] = []
+    var endCircleIndex: [Int] = []
+    var isPanned = false
     
-    func drawCircles(numberOf numberOfCircles: Int ,onRectangle rect: CGRect, withRadius radius: Double) {
+    func drawCircles(numberOf numberOfCircles: Int, onRectangle rect: CGRect, withRadius radius: Double) {
         self.radius = radius
         self.numberOfCircles = numberOfCircles
         self.draw(rect)
     }
     
     override func draw(_ rect: CGRect) {
+        self.layer.sublayers = nil
         let path = UIBezierPath()
-        let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
+        let center = CGPoint(x: (rect.width  / 2) - rect.minX, y: (rect.height / 2) - rect.minY)
         for i in 0 ... self.numberOfCircles {
             path.move(to: CGPoint(x: center.x + CGFloat(self.radius * Double(i)), y: center.y))
-            for j in stride(from: 0, to: 361.0, by: 1) {
-                let radians = j * Double.pi / 180
-                let x = Double(center.x) + self.radius * Double(i) * cos(radians)
-                let y = Double(center.y) + self.radius * Double(i) * sin(radians)
-                self.points.append((x,y))
-                path.addLine(to: CGPoint(x: x , y: y))
+            for j in 0 ... 362 {
+                let angle = Double (j) * Double.pi / 180
+                let x = CGFloat(Double(center.x) + self.radius * Double(i) * cos(angle))
+                let y = CGFloat(Double(center.y) + self.radius * Double(i) * sin(angle))
+                let point = CGPoint(x: x, y: y)
+                self.points.append(point)
+                path.addLine(to: point)
             }
+            endCircleIndex.append((i + 1) * 361)
         }
         
-        UIColor.white.setStroke()
-        path.lineWidth = 1
-        path.stroke()
+        path.close()
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0
+        animation.duration = 1
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.white.cgColor
+        shapeLayer.lineWidth = 4
+        shapeLayer.path = path.cgPath
+        if !(self.isPanned) {
+            shapeLayer.add(animation, forKey: "StrokeAnimations")
+        }
+        
+        self.layer.addSublayer(shapeLayer)
         self.backgroundColor = UIColor.clear
-        self.path = path
     }
 }
 
